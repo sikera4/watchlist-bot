dc744b1aba4e9ee698bd154c40607ae44e558442
andrey.sikera <andrey.sikera@onetwotrip.com>
2025-11-28 13:12:25 +0300

    use env

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,8 @@
 import { Bot } from 'grammy';
 
-const bot = new Bot('8586889965:AAGmmf2wxKYO2GCi6FZToYvYytw3AjC5U4g');
+const botToken = process.env.BOT_TOKEN;
+
+const bot = new Bot(botToken ?? '');
 
 bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
 bot.on('message', (ctx) => ctx.reply('Got another message!'));
