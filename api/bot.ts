cf420049d78c81c003cc29b56626339edba73495
andrey.sikera <andrey.sikera@onetwotrip.com>
2025-11-29 00:37:23 +0300

    add keyboard

--- api/bot.ts
+++ api/bot.ts
@@ -1,13 +1,20 @@
-import { Bot, webhookCallback } from "grammy";
+import { Bot, Keyboard, webhookCallback } from 'grammy';
 
 const token = process.env.BOT_TOKEN;
-if (!token) throw new Error("BOT_TOKEN is unset");
+if (!token) throw new Error('BOT_TOKEN is unset');
 
 const bot = new Bot(token);
 
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+const keyboard = new Keyboard()
+  .text('Получить мои списки для просмотра');
 
-const botWithWebhook: unknown = webhookCallback(bot, "https");
+bot.command('start', async (ctx) => {
+  await ctx.reply('Добро пожаловать! Что хотите сделать?', {
+    reply_markup: keyboard
+  })
+});
+bot.on('message', (ctx) => ctx.reply('Got another message!'));
+
+const botWithWebhook: unknown = webhookCallback(bot, 'https');
 
 export default botWithWebhook;
