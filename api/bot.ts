a80082c730f2e4833e92ecd712a087a616c72c97
andrey.sikera <andrey.sikera@onetwotrip.com>
2025-12-14 21:41:37 +0300

    remove open web app button from keyboard

--- api/bot.ts
+++ api/bot.ts
@@ -16,8 +16,7 @@ const bot = new Bot(token);
 const keyboard = new Keyboard()
   .text(MESSAGES.GET_MY_WATCHLISTS)
   .row()
-  .text(MESSAGES.CREATE_INVITE_LINK).row()
-  .webApp(MESSAGES.MANAGE_WATCHLISTS, WEB_APP_URL);
+  .text(MESSAGES.CREATE_INVITE_LINK);
 
 bot.command('start', async (ctx) => {
   const userId = ctx.from?.id;
